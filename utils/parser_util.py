0738abcc00832eff7f2c47231b13ed1b40957717
Zhao Yang <yangyz1230@gmail.com>
2023-11-21 01:34:07 +0800

    modify parse

--- utils/parser_util.py
+++ utils/parser_util.py
@@ -81,7 +81,7 @@ def add_diffusion_options(parser):
 
 def add_model_options(parser):
     group = parser.add_argument_group('model')
-    group.add_argument("--arch", default='trans_enc',
+    group.add_argument("--arch", default='past_cond',
                        choices=['trans_enc', 'trans_dec', 'gru', 'past_cond', 'inpainting'], type=str,
                        help="Architecture types as reported in the paper.")
     group.add_argument("--emb_trans_dec", default=False, type=bool,
