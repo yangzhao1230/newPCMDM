03aa47999be1109355112a28bd177b41f3783761
Zhao Yang <yangyz1230@gmail.com>
2023-11-21 01:16:14 +0800

    add model

---
+++ model/text_encoder.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+from transformers import RobertaTokenizer, RobertaModel
+from typing import List
+import torch
+import numpy as np
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+import torch.nn.functional as F
+import math
+from transformers import CLIPTokenizer, CLIPTextModel
+
+class TextEncoder(nn.Module):
+    def __init__(
+        self,
+        # clip_version="ViT-B/32"
+    ):
+        super().__init__()
+        # self.clip_version = clip_version
+        try:
+            self.clip_model = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
+            self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+        except:
+            self.clip_model = CLIPTextModel.from_pretrained("./clip")
+            self.tokenizer = CLIPTokenizer.from_pretrained("./clip")            
+
+    def forward(self, raw_text):
+
+        device = next(self.parameters()).device
+        text = self.tokenizer(raw_text, padding='max_length', return_tensors="pt", max_length=22, truncation=True)
+        input_ids = text["input_ids"]
+        attention_mask = text["attention_mask"]
+        output = self.clip_model(input_ids.to(device), attention_mask.to(device))['pooler_output'] 
+        return output
+    
+if __name__ == '__main__':
+    encoder = TextEncoder()
+    
